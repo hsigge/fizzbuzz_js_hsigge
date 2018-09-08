b4524bd708492e4560eb8068f742eb255e51617d
hsigge <hanna.sigge@gmail.com>
2018-09-08 15:00:16 +0200

    add test 'Buzz' for numbers that are divisible by 5

--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -1,5 +1,8 @@
 function FizzBuzz() {
     this.check = (number) => {
+        if (number % 5 === 0) {
+            return 'Buzz';
+        }
         if (number % 3 === 0) {
             return 'Fizz';
         } else {
