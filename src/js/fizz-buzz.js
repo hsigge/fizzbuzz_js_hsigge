5b051a6aa7c8741fba9f992170a37eac0c86faea
hsigge <hanna.sigge@gmail.com>
2018-09-09 19:54:05 +0200

    add else if replacement where appropriate

--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -3,10 +3,10 @@ function FizzBuzz() {
         if (number % 15 === 0) {
             return 'FizzBuzz';
         }
-        if (number % 5 === 0) {
+        else if (number % 5 === 0) {
             return 'Buzz';
         }
-        if (number % 3 === 0) {
+        else if (number % 3 === 0) {
             return 'Fizz';
         } else {
             return number
